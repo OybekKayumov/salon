1851a54a3506cc4562f02b6e9652cc10e9c722f9
Oybek Kayumov <oybek.kayumov@gmail.com>
2026-03-28 05:40:42 +0500

    Add Category Controller

---
+++ categoryservice/src/main/java/com/ok/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.ok.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping
+	public String home() {
+		return "Category Microservice for Salon Booking System";
+	}
+}
---
+++ categoryservice/src/main/java/com/ok/model/Category.java
@@ -0,0 +1,22 @@
+package com.ok.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private Long id;
+
+	@Column(unique = false)
+	private String name;
+
+	private String image;
+
+	private Long salonId;
+
+
+}
