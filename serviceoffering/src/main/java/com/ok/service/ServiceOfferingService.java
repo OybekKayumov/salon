057db2de74a291898c208089085fd11cef21d0a2
Oybek Kayumov <oybek.kayumov@gmail.com>
2026-03-29 06:55:21 +0500

    Add Service Offering Controller

---
+++ serviceoffering/src/main/java/com/ok/service/ServiceOfferingService.java
@@ -0,0 +1,22 @@
+package com.ok.service;
+
+import com.ok.model.ServiceOffering;
+import com.ok.payload.dto.CategoryDTO;
+import com.ok.payload.dto.SalonDTO;
+import com.ok.payload.dto.ServiceDTO;
+
+import java.util.List;
+import java.util.Set;
+
+public interface ServiceOfferingService {
+
+	ServiceOffering createService(SalonDTO salonDTO,
+	                              ServiceDTO serviceDTO,
+	                              CategoryDTO categoryDTO);
+
+	ServiceOffering updateService(Long serviceId, ServiceOffering service) throws Exception;
+
+	Set<ServiceOffering> getAllServiceBySalon(Long salonId, Long categoryId);
+
+	Set<ServiceOffering> getServicesByIds(Set<Long> ids);
+}
