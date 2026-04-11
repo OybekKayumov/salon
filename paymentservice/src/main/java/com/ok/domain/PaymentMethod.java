8ffd4074da2f7e3d48523189b3a70e8542d3ea3e
Oybek Kayumov <oybek.kayumov@gmail.com>
2026-04-11 06:40:09 +0500

    Add Payment Service Controller

---
+++ paymentservice/src/main/java/com/ok/domain/PaymentMethod.java
@@ -0,0 +1,7 @@
+package com.ok.domain;
+
+public enum PaymentMethod {
+
+	RAZORPAY,
+	STRIPE
+}
---
+++ paymentservice/src/main/java/com/ok/model/PaymentOrder.java
@@ -0,0 +1,35 @@
+package com.ok.model;
+
+import com.ok.domain.PaymentMethod;
+import com.ok.domain.PaymentOrderStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class PaymentOrder {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = false)
+	private Long amount;
+
+	@Column(nullable = false)
+	private PaymentOrderStatus status = PaymentOrderStatus.PENDING;
+
+	@Column(nullable=false)
+	private PaymentMethod paymentMethod;
+
+	private String paymentLinkId;
+
+	@Column(nullable=false)
+	private Long userId;
+
+	@Column(nullable=false)
+	private Long bookingId;
+
+	@Column(nullable=false)
+	private Long salonId;
+}
