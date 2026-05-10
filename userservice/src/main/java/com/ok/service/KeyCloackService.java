3ff8e8ea77541ab4724b0a1626e93cd9b583aeaf
Oybek Kayumov <oybek.kayumov@gmail.com>
2026-05-10 08:31:26 +0500

    Add keycloack

--- userservice/src/main/java/com/ok/service/KeyCloackService.java
+++ userservice/src/main/java/com/ok/service/KeyCloackService.java
@@ -7,5 +7,26 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class KeyCloackService {
 
+	private static final String KEYCLOACK_BASE_URL = "http://localhost:8080";
+
+	private static final String KEYCLOACK_ADMIN_API =
+					KEYCLOACK_BASE_URL + "/admin/realms/master/users";
+
+	private static final String TOKEN_URL =
+					KEYCLOACK_BASE_URL + "/realms/master/protocol/openid-connect/token";
+
+	private static final String CLIENT_ID = "salon-booking-client";
+	private static final String CLIENT_SECRET = "4TLgEBFTBG0bhsVNCmP5GWdws0IKJyv3";
+
+	private static final String GRANT_TYPE = "password";
+	private static final String scope = "openid email profile";
+
+	private static String username = "admin1";
+	private static String password = "123456";
+
+	private static final String clientId = "d016b90f-14c6-401e-899e-12d91f183543";
+
+
+
 
 }
